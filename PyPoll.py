dbba3b3a9a4887fc9cffba570dc34b19cc7a2b7d
KevinKronk <kjkronk@ncsu.edu>
2022-09-11 19:42:48 -0400

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -26,27 +26,44 @@ with open(file_to_load) as election_data:
             candidate_votes[voting_info[2]] = 0
         candidate_votes[voting_info[2]] += 1
 
-print(candidate_votes)
-
-# Loop through each candidates votes and calculate the total percentage
-for candidate, votes in candidate_votes.items():
-    vote_percentage = (votes / total_votes) * 100
-    print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
-    # Is the candidates votes greater than the current winning candidates votes 
-    if votes > winning_candidate[1]:
-        winning_candidate[0:3] = candidate, votes, vote_percentage
-
-# Print the winning candidate
-winning_candidate_summary = (
-    f"--------------------------\n"
-    f"Winner: {winning_candidate[0]}\n"
-    f"Winning Vote Count: {winning_candidate[1]:,}\n"
-    f"Winning Percentage: {winning_candidate[2]:.1f}%\n"
-    f"--------------------------\n")
-print(winning_candidate_summary)
+# print(candidate_votes)
 
 
 # Using the open() function with the "w" mode we will write data to the file
 with open(file_to_save, "w") as txt_file:
-    txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+    
+    # Set up header for election analysis text file
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    
+    # Write header to text file
+    txt_file.write(election_results)
+    
+    # Loop through each candidates votes and calculate the total percentage
+    for candidate, votes in candidate_votes.items():
+        vote_percentage = (votes / total_votes) * 100
+        candidate_results = (f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+       
+        # Write the candidate results: name, vote percentage, (votes) to the text file
+        print(candidate_results)
+        txt_file.write(candidate_results)
+
+        # Is the candidates votes greater than the current winning candidates votes 
+        if (votes > winning_candidate[1]) and (vote_percentage > winning_candidate[2]):
+            winning_candidate[0:3] = candidate, votes, vote_percentage
+
+    # Print the winning candidate
+    winning_candidate_summary = (
+        f"--------------------------\n"
+        f"Winner: {winning_candidate[0]}\n"
+        f"Winning Vote Count: {winning_candidate[1]:,}\n"
+        f"Winning Percentage: {winning_candidate[2]:.1f}%\n"
+        f"--------------------------\n")
+    print(winning_candidate_summary)
+    # Write winning candidate to text file
+    txt_file.write(winning_candidate_summary)
 
